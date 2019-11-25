fe752d40a620b690cd44a1b3df557fc535faec3f
cjw363 <asd6555363>
2019-11-25 16:36:53 +0800

    提交

--- facerecog-service/src/main/java/com/facerecog/config/StartupConfiguration.java
+++ facerecog-service/src/main/java/com/facerecog/config/StartupConfiguration.java
@@ -1,6 +1,9 @@
 package com.facerecog.config;
 
 import com.facerecog.service.interf.DeviceService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
@@ -16,25 +19,25 @@ import org.springframework.stereotype.Component;
  */
 @Component("StartupListener")
 public class StartupConfiguration implements ApplicationListener<ApplicationEvent> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StartupConfiguration.class);
     @Autowired
     DeviceService mDeviceService;
-
     /**
      * @see org.springframework.context.ApplicationListener#onApplicationEvent(org.springframework.context.ApplicationEvent)
      */
     @Override
     public void onApplicationEvent(ApplicationEvent event) {
         if(event instanceof ContextRefreshedEvent){//监听初始化
-            if(((ContextRefreshedEvent) event).getApplicationContext().getParent()!=null){
-                System.out.println("系统启动");
-            }
+//            if(((ContextRefreshedEvent) event).getApplicationContext().getParent()!=null){
+                LOGGER.info("系统启动");
+//            }
 
         }else if(event instanceof ContextClosedEvent){//监听销毁
-            if(((ContextClosedEvent) event).getApplicationContext().getParent()!=null){
+//            if(((ContextClosedEvent) event).getApplicationContext().getParent()!=null){
                 System.out.println("系统关闭");
                 //将设备在线状态修改为离线
                 mDeviceService.updateAllDeviceOffline();
-            }
+//            }
         }
     }
 }
