ee96fb9fd7f0fe5eb98703a847507a88cfe5fa44
cjw363 <asd6555363>
2020-06-28 17:46:29 +0800

    提交

--- facerecog-service/src/test/java/com/facerecog/test/RabbitMQTestRecv.java
+++ facerecog-service/src/test/java/com/facerecog/test/RabbitMQTestRecv.java
@@ -1,11 +1,12 @@
 package com.facerecog.test;
 
-import org.springframework.amqp.core.ExchangeTypes;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import com.rabbitmq.client.Channel;
+
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @Classname RabbitMQTestRecv
@@ -47,13 +48,40 @@ public class RabbitMQTestRecv {
 //        System.out.println("listen3 收到消息了:"+msg);
 //    }
 
-    @RabbitListener(bindings =
-    @QueueBinding(value = @Queue(value = TEST_QUEUE,durable = "true"),
-      exchange = @Exchange(value = TEST_EXCHANGE,
-        ignoreDeclarationExceptions = "true",
-        type = ExchangeTypes.DIRECT),
-    key = {"key_direct_static_page"}))
-    public void listen4(String msg){
-        System.out.println("listen4 收到消息了:"+msg);
+//    @RabbitListener(bindings =
+//    @QueueBinding(value = @Queue(value = TEST_QUEUE,durable = "true"),
+//      exchange = @Exchange(value = TEST_EXCHANGE,
+//        ignoreDeclarationExceptions = "true",
+//        type = ExchangeTypes.DIRECT),
+//    key = {"key_direct_static_page"}))
+//    public void listen4(String msg){
+//        System.out.println("listen4 收到消息了:"+msg);
+//    }
+
+    @RabbitListener(queues = TEST_QUEUE)
+    public void receiveMessage02(String msg, Channel channel, Message message) throws IOException {
+        try {
+            // 这里模拟一个空指针异常，
+            String string = null;
+            string.length();
+
+            System.out.println("【Consumer02成功接收到消息】>>> {}"+ msg);
+            // 确认收到消息，只确认当前消费者的一个消息收到
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (Exception e) {
+            if (message.getMessageProperties().getRedelivered()) {
+                System.out.println("【Consumer02】消息已经回滚过，拒绝接收消息 ： {}"+ msg);
+                // 拒绝消息，并且不再重新进入队列
+                //public void basicReject(long deliveryTag, boolean requeue)
+                channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+            } else {
+                System.out.println("【Consumer02】消息即将返回队列重新处理 ：{}"+ msg);
+                //设置消息重新回到队列处理
+                // requeue表示是否重新回到队列，true重新入队
+                //public void basicNack(long deliveryTag, boolean multiple, boolean requeue)
+                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+            }
+            e.printStackTrace();
+        }
     }
 }
--- facerecog-service/src/test/java/com/facerecog/test/RabbitMQTestSend.java
+++ facerecog-service/src/test/java/com/facerecog/test/RabbitMQTestSend.java
@@ -3,10 +3,15 @@ package com.facerecog.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.AmqpException;
-import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
 
 /**
  * @Classname RibbitmqTestSend
@@ -19,13 +24,45 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class RabbitMQTestSend {
     private static final String TEST_EXCHANGE = "facerecog.test.exchange";
 
+//    @Autowired
+//    private AmqpTemplate mAmqpTemplate;
     @Autowired
-    private AmqpTemplate mAmqpTemplate;
+    private RabbitTemplate mRabbitTemplate;
 
     @Test
     public void sendMsg(){
         try {
-            mAmqpTemplate.convertAndSend(TEST_EXCHANGE,"","hello");
+            /**
+             * 如果消息没有到达交换机,则该方法中isSendSuccess = false,error为错误信息;
+             * 如果消息正确到达交换机,则该方法中isSendSuccess = true;
+             */
+            mRabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+                @Override
+                public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                    System.out.println("消息id:" + correlationData.getId());
+                    if (ack) {
+                        System.out.println("消息发送确认成功");
+                    } else {
+                        System.out.println("消息发送确认失败:" + cause);
+                    }
+                }
+            });
+            /**
+             * 消息从交换机成功到达队列，则returnedMessage方法不会执行;
+             * 消息从交换机未能成功到达队列，则returnedMessage方法会执行;
+             */
+            mRabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+                @Override
+                public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                    try {
+                        System.out.println("return--message:" + new String(message.getBody(), "UTF-8") + ",replyCode:" + replyCode
+                          + ",replyText:" + replyText + ",exchange:" + exchange + ",routingKey:" + routingKey);
+                    } catch (UnsupportedEncodingException e) {
+                    }
+                }
+            });
+            CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+            mRabbitTemplate.convertAndSend(TEST_EXCHANGE,"","hello",correlationData);
         } catch (AmqpException e) {
             e.printStackTrace();
         }
@@ -34,7 +71,7 @@ public class RabbitMQTestSend {
     @Test
     public void sendCreateStaticPageMsg(){
         try {
-            mAmqpTemplate.convertAndSend(TEST_EXCHANGE,"key_direct_static_page","1");
+            mRabbitTemplate.convertAndSend(TEST_EXCHANGE,"key_direct_static_page","1");
         } catch (AmqpException e) {
             e.printStackTrace();
         }
